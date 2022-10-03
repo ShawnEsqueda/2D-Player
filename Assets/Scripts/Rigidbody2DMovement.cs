02c9730d70c8c70e28c4b6b976134e899680cc09
ShawnEsqueda <shawnesqueda@icloud.com>
2022-10-03 10:40:04 -0700

    Initial commit

---
+++ Assets/Scripts/Rigidbody2DMovement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rigidbody2DMovement : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    public float jumpForce = 10;
+
+    public bool isOnGround;
+
+    private Rigidbody2D _myRB;
+    private Collider2D _myCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       _myRB = GetComponent<Rigidbody2D>();
+       _myCollider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+
+        PlayerJump();
+    }
+
+    void PlayerMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        _myRB.velocity = new Vector2(horizontalInput * moveSpeed, _myRB.velocity.y);
+    }
+
+    void PlayerJump()
+    {
+        if(_myCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            isOnGround = true;
+        }
+        else{
+            isOnGround = false;
+        }
+
+        if(Input.GetButtonDown("Jump") && isOnGround)
+        {
+            _myRB.velocity = new Vector2(_myRB.velocity.x, jumpForce);
+        }
+    }
+}
---
+++ Assets/Scripts/TransformMovementRotation.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformMovementRotation : MonoBehaviour
+{
+    public float speed = 5;
+    public float rotationSpeed = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+        transform.Rotate(Vector3.forward, verticalInput * rotationSpeed * Time.deltaTime);
+    }
+}
